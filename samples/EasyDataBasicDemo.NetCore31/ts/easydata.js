0b70300c2c0585379747d2f0bcbfb00d250672ab
Anton Ivanenko <anton_ivanenko@outlook.com>
2020-11-17 19:53:30 +0200

    Add .NET 5 sample

---
+++ samples/EasyDataBasicDemo.NetCore31/ts/easydata.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var crud_1 = require("@easydata/crud");
+window.addEventListener('load', function () {
+    new crud_1.EasyDataViewDispatcher().run();
+});
--- samples/EasyDataBasicDemo/Pages/Index.cshtml.cs
+++ samples/EasyDataBasicDemo/Pages/Index.cshtml.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace EasyDataBasicDemo.Pages
 {
