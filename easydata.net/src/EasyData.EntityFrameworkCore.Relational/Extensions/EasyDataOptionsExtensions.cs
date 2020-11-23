3ea9ed263cee919ac1397d60ca7e1a1f1bc7cf8d
Sergiy Korzh <sergey.korzh@live.com>
2020-11-23 13:48:21 +0200

    [Upd] A possibility to set loader options on UseDbContext call

--- easydata.net/src/EasyData.EntityFrameworkCore.Relational/Extensions/EasyDataOptionsExtensions.cs
+++ easydata.net/src/EasyData.EntityFrameworkCore.Relational/Extensions/EasyDataOptionsExtensions.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using EasyData.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyData.Services
 {
     public static class EasyDataOptionsExtensions
     {
-        public static void UseDbContext<TDbContext>(this EasyDataOptions options) where TDbContext : DbContext
+        public static void UseDbContext<TDbContext>(this EasyDataOptions easyDataOptions, Action<DbContextMetaDataLoaderOptions> loaderOptionsBuilder = null) where TDbContext : DbContext
         {
-            options.UseManager<EasyDataManagerEF<TDbContext>>();
+            if (loaderOptionsBuilder != null) {
+                easyDataOptions.MetaDataLoaderOptionsBuilder = (options) => loaderOptionsBuilder(options as DbContextMetaDataLoaderOptions);
+            }
+            easyDataOptions.UseManager<EasyDataManagerEF<TDbContext>>();
         }
     }
 }
