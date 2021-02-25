3794e803529e41bb4151c4e60af0609e20af141b
Anton Ivanenko <anton_ivanenko@outlook.com>
2021-02-25 14:57:22 +0200

    Fix HtmlDataExportSettings and ExcelHtmlDataExportSettings names

---
+++ easydata.net/src/EasyData.Exporters.Default/ExcelHtml/ExcelHtmlDataExportSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+using EasyData.Export.Html;
+
+namespace EasyData.Export.Excel
+{
+    /// <summary>
+    /// Represents different settings used during exporting to Excel-HTML
+    /// </summary>
+    public class ExcelHtmlDataExportSettings : HtmlDataExportSettings
+    {
+        /// <summary>
+        /// Returns the default instance of <see cref="ExcelHtmlDataExportSettings"/>.
+        /// </summary>
+        public new static HtmlDataExportSettings Default => new HtmlDataExportSettings();
+
+        /// <summary>
+        /// Initializes a new instance of the<see cref="ExcelHtmlDataExportSettings"/> class.
+        /// </summary>
+        public ExcelHtmlDataExportSettings() : base() 
+        {
+        
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the<see cref="ExcelHtmlDataExportSettings"/> class.
+        /// </summary>
+        public ExcelHtmlDataExportSettings(CultureInfo culture) : base(culture)
+        {
+
+        }
+    }
+}
