7c14a968f44c78cb9565d1bc07501cbc5899ee1b
Oleksandr Melnychenko <oleksandr.melnychenko@fts.com.ua>
2021-06-29 19:28:12 +0300

    Task #3087 - option to change the page size

--- easydata.js/packs/ui/src/i18n/text_resources.ts
+++ easydata.js/packs/ui/src/i18n/text_resources.ts
@@ -4,6 +4,7 @@ import { i18n } from '@easydata/core';
 function addEasyDataUITexts() {
     i18n.updateDefaultTexts({
         GridPageInfo: '{FirstPageRecordNum} - {LastPageRecordNum} of {Total} records',
+        GridItemsPerPage: 'items per page',
         ButtonOK: "OK",
         ButtonCancel: "Cancel",
         ButtonApply: 'Apply',
