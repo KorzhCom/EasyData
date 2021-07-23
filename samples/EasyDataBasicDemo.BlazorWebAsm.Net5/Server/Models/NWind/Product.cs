ebbc8308efed9a320663d125928699d9b02bb0ed
Anton Ivanenko <anton_ivanenko@outlook.com>
2021-07-23 18:41:09 +0300

    Add blazor samples

---
+++ samples/EasyDataBasicDemo.BlazorWebAsm.Net5/Server/Models/NWind/Product.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyDataBasicDemo.Models
+{
+    [DisplayColumn("Name")]
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("ProductID")]
+        public int Id { get; set; }
+
+        [Column("ProductName")]
+        public string Name { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int? SupplierID { get; set; }
+
+
+        [ForeignKey("SupplierID")]
+        public virtual Supplier Supplier { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public short? UnitsInStock { get; set; }
+
+        public short? UnitsOnOrder { get; set; }
+
+        public short? ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+
+
+}
