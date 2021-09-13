eda0e3b55adfa5e618fce2325d1333455412b6e1
Anton Ivanenko <anton_ivanenko@outlook.com>
2021-09-13 23:35:00 +0300

    Add playground .NET6 sample. Update packages to support .NET 6

---
+++ playground/EasyDataAspNetCoreTest03/Models/NWind/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+using EasyData.EntityFrameworkCore;
+
+namespace EasyDataBasicDemo.Models
+{
+    [MetaEntity(Description = "Categories of Product")]
+    public class Category
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("CategoryID")]
+        public int Id { get; set; }
+  
+        [MetaEntityAttr(ShowOnCreate = false, ShowOnEdit = false, ShowOnView = false)]
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        [ScaffoldColumn(false)]
+        public byte[] Picture { get; set; } 
+
+    }
+}
