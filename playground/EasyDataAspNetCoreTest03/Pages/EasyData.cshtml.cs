95ed7a94e0698b7d98a60be8e21585538d0a02d9
Anton Ivanenko <anton_ivanenko@outlook.com>
2021-09-13 23:35:00 +0300

    Add playground .NET6 sample. Update packages to support .NET 6

---
+++ playground/EasyDataAspNetCoreTest03/Pages/EasyData.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyDataBasicDemo.Pages
+{
+    public class EasyDataModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
