51a9fec333d0467459e80c3789a0952405da9a21
Sergiy Korzh <sergey.korzh@live.com>
2021-10-14 11:28:05 +0300

    Update setup_dev.bat

--- playground/setup_dev.bat
+++ playground/setup_dev.bat
@@ -1,8 +1,28 @@
 cd .\EasyDataAspNetCoreTest01 
 call npm install 
 
-cd ..\EasyDataAspNetCoreTest02 
+cd node_modules
+mkdir @easydata
+cd @easydata
+
+mklink /D core ..\..\..\..\easydata.js\packs\core
+mklink /D ui ..\..\..\..\easydata.js\packs\ui
+mklink /D crud ..\..\..\..\easydata.js\packs\crud
+
+
+cd ..\..\..
+
+
+cd .\EasyDataAspNetCoreTest02 
 call npm install 
+cd node_modules
+mkdir @easydata
+cd @easydata
+
+mklink /D core ..\..\..\..\easydata.js\packs\core
+mklink /D ui ..\..\..\..\easydata.js\packs\ui
+mklink /D crud ..\..\..\..\easydata.js\packs\crud
+cd ..\..\..
+
 
 cd ..
-
