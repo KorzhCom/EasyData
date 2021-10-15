58afa72ac10823567c15208cc151fa626e90dc3f
Sergiy Korzh <sergey.korzh@live.com>
2021-10-15 12:21:08 +0300

    Add IEasyCache interface

---
+++ easydata.net/src/EasyData.Core/Services/IEasyCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyData.Services
+{
+    /// <summary>
+    ///  Represents a general caching service
+    /// </summary>
+    public interface IEasyCache
+    {
+        /// <summary>
+        /// Gets the value associated with this key if present.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>System.String.</returns>
+        string GetValue(string key);
+
+        /// <summary>
+        /// Puts the key:value pair to the cache
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        void PutValue(string key, string value);
+    }
+}
