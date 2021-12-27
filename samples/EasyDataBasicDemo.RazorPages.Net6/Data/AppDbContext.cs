60f08390e7698cb525a2a5cf95e60bb07fbe127f
Sergiy Korzh <sergey.korzh@live.com>
2021-12-27 17:49:00 +0200

    EasyData sample for .NET 6 with minimal API

---
+++ samples/EasyDataBasicDemo.RazorPages.Net6/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+using EasyDataBasicDemo.Models;
+
+namespace EasyDataBasicDemo
+{    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<Shipper> Shippers { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<OrderDetail>()
+                .ToTable("Order_Details")
+                .HasKey(od => new { od.OrderID, od.ProductID });
+        }
+    }
+}
