83850940a95b96137b5a4f05081d930145f776e8
EasyQuery team lead <easyquery@korzh.com>
2022-09-10 11:29:46 +0300

    Dashboard page

---
+++ playground/EasyDataAspNetCoreTest01/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyDataBasicDemo.Pages
+{
+    public class EasyDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
