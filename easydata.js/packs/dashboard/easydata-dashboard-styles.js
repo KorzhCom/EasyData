6fc4c6d8b06f62b9efb1ec9c1debb0b29b1e0cf1
Oleksandr Melnychenko <green@crocodile.in.ua>
2022-10-28 15:37:30 +0300

    Fix dashboard.css path for webpack

--- easydata.js/packs/dashboard/easydata-dashboard-styles.js
+++ easydata.js/packs/dashboard/easydata-dashboard-styles.js
@@ -1,4 +1,4 @@
 require('../ui/dist/assets/css/easy-grid.css');
 require('../ui/dist/assets/css/easy-dialog.css');
 require('../ui/dist/assets/css/easy-forms.css');
-require('../dashboards/dist/assets/css/dashboard.css');
+require('./src/assets/css/dashboard.css');
