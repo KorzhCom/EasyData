00749e19832767a516e1c500385df0870d05f715
olton <serhii@pimenov.com.ua>
2022-12-25 11:43:15 +0200

    added line chart

--- easydata.js/packs/dashboard/src/widgets/chart.ts
+++ easydata.js/packs/dashboard/src/widgets/chart.ts
@@ -5,33 +5,54 @@ import {domel} from "@easydata/ui";
 declare var Chart
 declare var google
 
-const cheChartJS = () => {
+const checkChartJS = () => {
     if (typeof Chart === "undefined") {
         panic(`ChartJS library required!`)
     }
 }
-const createBarPieChartJSChart = (ctx, {axisX, axisY}, options) => {
-    cheChartJS()
-    
-    options.data.labels = axisX
-    options.data.datasets[0].data = axisY
 
-    return new Chart(ctx, options)
-}
+const createChartJSChart = (ctx, type, data, options) => {
+    checkChartJS()
 
-const createLineChartJSChart = (ctx, {axisX, axisY}, options) => {
-    cheChartJS()
+    switch (type) {
+        case 'bubble': {
+            const data = []
 
-    const {indexAxis = 'x'} = options
-    
-    if (indexAxis === 'x') {
-        options.data.labels = axisX
-        options.data.datasets[0].data = axisY        
-    } else {
-        options.data.labels = axisY
-        options.data.datasets[0].data = axisX
+            data["axisX"].forEach((el, index)=>{
+                data.push({
+                    x: el,
+                    y: data["axisY"][index],
+                    r: data["axisZ"][index],
+                })
+            })
+            
+            options.datasets.data = data
+            
+            break
+        }
+        case 'line': {
+            const {indexAxis = 'x'} = options
+
+            if (indexAxis === 'x') {
+                options.data.labels = data["axisX"]
+                options.data.datasets[0].data = data["axisY"]
+            } else {
+                options.data.labels = data["axisY"]
+                options.data.datasets[0].data = data["axisX"]
+            }
+            
+            break
+        }
+        case 'pie':
+        case 'bar':
+        case 'doughnut': {
+            options.data.labels = data["axisX"]
+            options.data.datasets[0].data = data["axisY"]
+            
+            break
+        }
     }
-
+    
     return new Chart(ctx, options)
 }
 
@@ -39,11 +60,12 @@ export class EasyChart extends EasyDataWidget {
     constructor(elem: HTMLElement, widget: TWidget) {
         super(elem, "EasyChart", widget.options);
 
-        const axisX = [], axisY = []
+        const axisX = [], axisY = [], axisZ = []
         const data = widget.dataset.resultSet.rows
         for (let r of data) {
             axisX.push(r[widget.axis.x])
             axisY.push(r[widget.axis.y])
+            axisZ.push(r[widget.axis.z])
         }
 
         if (widget.lib.toLowerCase() === 'chartjs') {
@@ -51,18 +73,16 @@ export class EasyChart extends EasyDataWidget {
             const ctx = canvas.getContext("2d")
             elem.appendChild(canvas)
 
-            switch (widget.options.type) {
-                case "bar":
-                case "pie":
-                case "doughnut": {
-                    createBarPieChartJSChart(ctx,{axisX, axisY}, widget.options)
-                    break;
-                }
-                case "line": {
-                    createLineChartJSChart(ctx,{axisX, axisY}, widget.options)
-                    break;
-                }
-            }
+            createChartJSChart(
+                ctx, 
+                widget.options.type,
+                {
+                    axisX, 
+                    axisY, 
+                    axisZ
+                }, 
+                widget.options
+            )
         } else {
 
         }
