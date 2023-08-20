da143668beb8280a7943d11b46b47fa1a281973c
Oleksandr Melnychenko <green@crocodile.in.ua>
2023-08-20 13:50:15 +0300

    Add script to sample

--- playground/EasyDataAspNetCoreTest01/ts/easydata.ts
+++ playground/EasyDataAspNetCoreTest01/ts/easydata.ts
@@ -3,14 +3,8 @@ import { showTimeSpanPicker } from '@easydata/ui'
 
 window.addEventListener('load', () => {
     console.log(showTimeSpanPicker)
-    new EasyDataViewDispatcher({
-        basePath: 'crud',
-        endpoint: '/api/easy-crud',
-        showFilterBox: true
-        //dataTable: {
-        //    elasticChunks: true
-        //}
-        // rootEntity: 'Customer'
-    }).run()
 });
 
+globalThis.showPicker = () => {
+    alert('showTimeSpanPicker');
+}
