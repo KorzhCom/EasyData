ac8d92a44f040c3b9a4b4a09b31787f7fd8178fd
olton <serhii@pimenov.com.ua>
2023-10-30 19:04:30 +0200

    rollup

--- playground/EasyDataAspNetCoreTest01/ts/styles.js
+++
@@ -1,4 +0,0 @@
-﻿require('../node_modules/@easydata/ui/dist/assets/css/easy-grid.css');
-require('../node_modules/@easydata/ui/dist/assets/css/easy-dialog.css');
-require('../node_modules/@easydata/ui/dist/assets/css/easy-forms.css');
-require('../node_modules/@easydata/crud/dist/assets/css/ed-view.css');
