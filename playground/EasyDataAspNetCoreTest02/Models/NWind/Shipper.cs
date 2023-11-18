d929913ccb5c2f5823e48eb950b9a22dbae0b65c
Sergiy Korzh <sergey.korzh@live.com>
2023-11-18 20:07:56 +0200

    Version 1.4.19 - .NET 8.0 support

---
+++ playground/EasyDataAspNetCoreTest02/Models/NWind/Shipper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyDataBasicDemo.Models
+{
+    public class Shipper
+    {
+        [Column("ShipperID")]
+        public int Id { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
