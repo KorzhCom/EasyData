30095c859dd39b7f9ce21ab29a9a7b0db6b3d2a8
olton <serhii@pimenov.com.ua>
2023-12-25 15:58:16 +0200

    add caching

--- playground/EasyDataAspNetCoreTest01/ts/styles.js
+++ playground/EasyDataAspNetCoreTest01/ts/styles.js
@@ -1,4 +1,4 @@
-﻿import '../node_modules/@easydata/ui/dist/assets/css/easy-grid.css'
-import '../node_modules/@easydata/ui/dist/assets/css/easy-dialog.css'
-import '../node_modules/@easydata/ui/dist/assets/css/easy-forms.css'
-import '../node_modules/@easydata/crud/dist/assets/css/ed-view.css'
+﻿import '@easydata/ui/dist/assets/css/easy-grid.css'
+import '@easydata/ui/dist/assets/css/easy-dialog.css'
+import '@easydata/ui/dist/assets/css/easy-forms.css'
+import '@easydata/crud/dist/assets/css/ed-view.css'
