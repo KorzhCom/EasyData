fa0b2b4a6d8ca84c3c59e0e81d3612cc48eebd7b
Sergiy Korzh <sergey.korzh@live.com>
2024-01-21 19:03:35 +0200

    Add EasyDataException

---
+++ easydata.net/src/EasyData.Core/Common/EasyDataException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyData
+{
+    public class EasyDataException : Exception
+    {
+        public EasyDataException(string message) : base(message)
+        {
+        }
+
+        public EasyDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
