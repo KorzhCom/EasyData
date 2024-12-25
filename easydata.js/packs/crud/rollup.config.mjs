023c4c40dedc006d16e6c5922d615e38d0b9fced
Serhii Pimenov <serhii@pimenov.com.ua>
2024-12-25 23:45:15 +0200

    add build time to banners

--- easydata.js/packs/crud/rollup.config.mjs
+++ easydata.js/packs/crud/rollup.config.mjs
@@ -22,6 +22,7 @@ const production = !(process.env.ROLLUP_WATCH),
 const banner = `
 /*
  * EasyData.JS CRUD v${pkg.version}
+ * Build time: ${new Date().toLocaleString()}
  * Copyright 2020-${new Date().getFullYear()} Korzh.com
  * Licensed under MIT
  */
